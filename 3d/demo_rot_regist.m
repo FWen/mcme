8b87b43af810ed2d961bfeb6f3cf29ce48964ca1
FWen <wenfei@sjtu.edu.cn>
2020-07-16 22:20:29 +0800

    Add files via upload

---
+++ 3d/demo_rot_regist.m
@@ -0,0 +1,74 @@
+clear all; close all; clc;
+
+addpath data common ../minFunc ../minFunc/compiled
+
+load('data_stanford_1')
+
+% normalizing to [0, 1]^3 and downsampling
+pc_o = double(conf.v1.Location);
+pc_o = pc_o - repmat(min(pc_o),size(pc_o,1),1);
+pc_o = pointCloud(pc_o./repmat(max(pc_o),size(pc_o,1),1));
+
+N = 100;
+sigma = 0.1;
+outlier_ratios = [0:0.2:0.6, 0.7, 0.8, 0.9, 0.92, 0.95];
+
+for n=1:length(outlier_ratios)
+    for k=1:50
+        
+        [outlier_ratios(n), k]
+        
+        J = randperm(pc_o.Count);
+        kpc1 = pointCloud(pc_o.Location(J(1:N),:));
+
+        rng('shuffle')
+        G = affine3d([randrot, zeros(3,1); zeros(1,3), 1]); % rotation
+
+        kpc2 = pctransform(kpc1,G);
+        
+        
+        %% noise and outlier generation
+        n_outl = round(N*outlier_ratios(n));
+        pt1 = double(kpc1.Location);
+        pt2 = double(kpc2.Location) + randn(size(pt1))*sigma;
+        J = randperm(N);
+        pt2(J(1:n_outl),:) = rand(n_outl,3)*5.0;
+        
+
+        %% RANSAC
+        RANSAC_TIMEOUT = 60;
+        [nInliers, T, iter, ~] = ransac3dof_timeout(pt1, pt2, 2*sigma, .999, RANSAC_TIMEOUT);
+        T = T'; 
+        angErr_rs(k,n) = 2*asin(norm(G.T(1:3,1:3)-T(1:3,1:3),'fro')/(2*sqrt(2))); 
+        q_ransac = dcm2quat(T(1:3,1:3)); 
+
+        
+        %% MCME
+        [R] = mcme_rot_regist(pt1, pt2, sigma, q_ransac);
+        angErr_mcme(k,n) = 2*asin(norm(G.T(1:3,1:3)-R,'fro')/(2*sqrt(2)));  
+    end
+end
+
+
+%% rotation error
+m=2;
+x = [angErr_rs; angErr_mcme]/pi*180;
+g1 = kron([1:m]', ones(size(angErr_rs))); g1 = g1(:);
+g2 = repmat(1:n,size(x,1),1); g2 = g2(:);
+
+f=figure(1);
+bh=boxplot(x(:), {g2,g1},'whisker',1,'colorgroup',g1, 'factorgap',[m, 1],'symbol','.','outliersize',4,'widths',0.6,...
+    'factorseparator',[1],'colors','rgbm');
+xlabel('Outlier ratio');
+ylabel('Rotation error (deg)');
+set(gca,'yscale','log');grid on;%ylim([1e-3,3e0]);
+set(bh,'linewidth',1.5);
+set(findobj(gca,'tag','Outliers'),'Marker','+');  
+
+for k=1:size(angErr_rs,2)
+    annotation(f,'textbox',[((k-1)+1.5)/(size(angErr_rs,2)+2), 0.075, 0.035, 0.075],...
+    'String',{outlier_ratios(k)},'FitBoxToText','off', 'EdgeColor','none');
+end
+set(gca,'XTickLabel',{' '})
+box_vars = findall(gca,'Tag','Box');
+hLegend = legend(box_vars([m:-1:1]), {'RANSAC','GORE-RANSAC','MCME'}, 'Location', 'Best');
---
+++ 3d/quasar.m
@@ -0,0 +1,51 @@
+function [R, q] = quasar(X, Y, sigma)
+
+numOfPts = size(X,1);
+Q = zeros(4*numOfPts+4);
+% cbar2 = 1.;
+noise_bound = chi2inv(1-1e-6,3)*sigma*sigma;
+EYE4 = eye(4);
+
+for k=1:numOfPts
+    a = X(k,:);
+    b = Y(k,:);
+    
+    RES = (a*a.'+b*b.')*EYE4 + 2*O12p([b,0], [a,0]);
+    Q_kk = RES/noise_bound/2 + EYE4/2;
+    Q_0k = RES/noise_bound/4 - EYE4/4;
+    Q(k*4+(1:4),k*4+(1:4)) = Q_kk;
+    Q(k*4+(1:4),(1:4)) = Q_0k; 
+    Q((1:4),k*4+(1:4)) = Q_0k; 
+end
+
+
+%% solve SDP
+cvx_begin quiet
+    cvx_solver mosek%sdpt3
+    variable Z(4*numOfPts+4, 4*numOfPts+4) symmetric
+    minimize trace(Q*Z)
+    subject to
+    trace(Z(1:4,1:4)) == 1;
+    for k = 1:numOfPts
+        Z(k*4+(1:4),k*4+(1:4)) == Z(1:4,1:4);  
+    end
+    for k = 1:numOfPts+1
+        for l = k+1:numOfPts+1
+            %Z((k-1)*4+(1:4),(l-1)*4+(1:4)) == Z((k-1)*4+(1:4),(l-1)*4+(1:4)).'; 
+            Z((k-1)*4+1,(l-1)*4+(2:4)) == Z((k-1)*4+(2:4),(l-1)*4+1).';
+            Z((k-1)*4+2,(l-1)*4+(3:4)) == Z((k-1)*4+(3:4),(l-1)*4+2).';
+            Z((k-1)*4+3,(l-1)*4+4)     == Z((k-1)*4+4,(l-1)*4+3);
+        end
+    end
+    Z == semidefinite(4*numOfPts+4);
+cvx_end
+
+
+%% eigenvalue decomposition
+[V, D] = eig(Z); 
+[~, idxmax] = max(diag(D)); 
+q1 = V(:,idxmax);
+
+R = quat2dcm([q1(4); q1(1:3)]'); 
+q = quatnormalize([q1(4); q1(1:3)]');
+
